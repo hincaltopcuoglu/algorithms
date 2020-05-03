3903aebf4a03ce82839feea69a5e9c4830248c24
hincaltopcuoglu <hincal@topcuoglu.me>
2020-05-03 07:29:19 +0300

    bubble sort

---
+++ algorithms7/mainx.py
@@ -0,0 +1,17 @@
+def bubble_sort():
+    f = open("input", 'r')
+    nums = f.readlines()
+    nums = [int(i) for i in nums]
+    n = len(nums)
+    for i in range(len(nums)):
+        for j in range(0,n-i-1):
+            if nums[j]>nums[j+1]:
+                nums[j+1], nums[j] = nums[j], nums[j+1]
+
+    return nums
+
+if __name__=='__main__':
+    print(bubble_sort())
+
+
+
