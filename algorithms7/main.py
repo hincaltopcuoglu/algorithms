2bfb7e13cc427bdea9dcf1f69f99b96c6a6f43c8
hincaltopcuoglu <hincal@topcuoglu.me>
2020-05-03 07:31:40 +0300

    bubble sort (file organized)

--- algorithms7/main.py
+++ algorithms7/main.py
@@ -3,7 +3,7 @@ def bubble_sort():
     nums = f.readlines()
     nums = [int(i) for i in nums]
     n = len(nums)
-    for i in range(len(nums)):
+    for i in range(n):
         for j in range(0,n-i-1):
             if nums[j]>nums[j+1]:
                 nums[j+1], nums[j] = nums[j], nums[j+1]
