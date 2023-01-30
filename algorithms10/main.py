64dfcba1b73571b0b86876a8ac72b19f3661edf5
htopc <hincal.topcuoglu@gmail.com>
2023-01-30 23:16:17 +0300

    find the prime numbers between 10 to 100

---
+++ algorithms10/main.py
@@ -0,0 +1,13 @@
+## find the prime numbers between 10 to 100
+
+for i in range(10,101):
+    if (i % 2) == 0:
+        pass
+    elif (i % 3) == 0:
+        pass
+    elif (i % 5) == 0:
+        pass
+    elif (i % 7) == 0:
+        pass
+    else:
+        print(i)
