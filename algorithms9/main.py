dd682fb596ba069e7e46952f36d357373068ee61
htopc <htopc@slomins.com>
2023-01-30 22:56:20 +0300

    product process of integers from 1 to 10

---
+++ algorithms9/main.py
@@ -0,0 +1,10 @@
+## find all product of numbers between 1 to 11
+
+for i in range(1,11):
+    if i==1:
+        product = 1
+    else:
+        product = product * i
+        i += 1
+        if i == 11:
+            print(product)
