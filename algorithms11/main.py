e4e5592aaac07f017da7a82c8bfff957400ca04b
hincaltopcuoglu <hincal.topcuoglu@gmail.com>
2023-02-02 18:48:03 +0300

    function changes to dividens

--- algorithms11/main.py
+++ algorithms11/main.py
@@ -1,13 +1,19 @@
 
 #algorithm that finds the difference between the value in the hundreds digit of an entered 4-digit integer 
 # and the remainder obtained by dividing by 3 with the value in the ones digit of the same number
+input = input("Enter your value: ")
 
-val = input("Enter your value: ")
-val = int(val)
-thou = int(val / 1000) * 1000
-hundr = val - thou
-value = int(hundr / 100) * 100
-s = value - int(value / 3) * 3
-ones = val - int(val / 10) * 10
-diff = s - ones
-print(diff)
+def dividends(input):
+    val = input
+    val = int(val)
+    thou = int(val / 1000) * 1000
+    hundr = val - thou
+    value = int(hundr / 100) * 100
+    s = value - int(value / 3) * 3
+    ones = val - int(val / 10) * 10
+    diff = s - ones
+    return(print(diff))
+
+
+if __name__ == "__main__":
+    dividends(input)
